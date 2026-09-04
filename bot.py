7589c1cf9e8eca7fc52f7abac984f17d7677d37a
sfsmikroskil <sfsmikroskil@gmail.com>
2026-09-04 14:59:45 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,7 +37,7 @@ async def main(nama, email, c):
         await page.check("input.form-check-input")
         
         await page.wait_for_timeout(1000)
-        if(c==0 or True):
+        if(c==0):
             await page.screenshot(path=f"{c}_0reg.png")
         # 3. Regis
         await page.click("button[type='submit']")
@@ -59,29 +59,34 @@ async def main(nama, email, c):
         # 6. Masukk Hall
         await page.wait_for_timeout(2000)
         await page.mouse.click(277, 654)
-        await page.wait_for_timeout(1000)
+        await page.wait_for_timeout(5000)
         #await page.screenshot(path=f"{c}_4hall.png")
         # 7. Filter booth
         await page.mouse.click(420, 30)
         await page.wait_for_timeout(1000)
         #await page.screenshot(path=f"{c}_5filter.png")
-        await page.keyboard.type("UKPBJ KEMENTERIAN IM")
-        await page.wait_for_timeout(1000)
+        await page.keyboard.type("imi")
+        await page.wait_for_timeout(2000)
         #await page.screenshot(path=f"{c}_6booth.png")
         await page.keyboard.press("Enter")
-        await page.wait_for_timeout(2000)
+        await page.wait_for_timeout(1000)
+        await page.mouse.click(277, 654)
         await page.keyboard.press("Enter")
         await page.wait_for_timeout(2000)
-        await page.mouse.click(170, 662)
-        await page.wait_for_timeout(2000)
-        if(c==0 or True):
+        #await page.mouse.click(357, 624) #video
+        #await page.mouse.click(170, 662) #poster kiri
+        #await page.mouse.click(540, 662) #poster kanan
+        #await page.wait_for_timeout(2000)
+        #await page.mouse.click(170, 662) 
+        #await page.wait_for_timeout(2000)
+        if(c==0):
             await page.screenshot(path=f"{c}_7booth.png")
 
         print(f"Akun : ({c}) {nama} | Selesai")
         await browser.close()
 
 if __name__ == "__main__":
-    jumlah = 500
+    jumlah = 1
     mulaiDari = 0
     print("Mulai...")
     for i in range (mulaiDari, mulaiDari+jumlah):
